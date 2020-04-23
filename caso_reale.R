ef04fda310c132fe074452d0143750ab752f03ab
AngusFangus <45711317+AngusFangus@users.noreply.github.com>
2020-04-23 20:00:56 +0200

    Update caso_reale.R

--- caso_reale.R
+++ caso_reale.R
@@ -267,11 +267,11 @@ mse_lars
 x_standardizer <- standardizer(x_train)
 y_standardizer <- standardizer(y_train)
 mod_lars2 <- lars(x_standardizer(x_train), y_standardizer(y_train))
-mse_lars2 = c()
+mse_lars2 <- c()
 beta_compl <- mod_lars2$log
 beta_sing <- mod_lars2$coef
 x_test_stand <- x_standardizer(x_test)
-I = ncol(beta_compl)
+I <- ncol(beta_compl)
 
 for (i in 1:I) {
   yhat_l2 <- x_test_stand %*% beta_compl[,i]
@@ -282,8 +282,7 @@ for (i in 1:I) {
 mse_lars2 # come varia l'errore ad ogni iterazione
                      
 
-n <- nrow(real2)
-K = 5
+K <- 5
 folds <- sample( rep(1:K,length=n) )
 KCV <- matrix(NA,K,100)
 
@@ -333,270 +332,48 @@ ggplot() + geom_point( aes(x = 1:50, y = KCVmean), color='red', lwd=2.3) +
                      
                      
                      
-                     
-                     
-                     
 
-#R2ADJ
-n_test <- nrow(y_test)
-p <- ncol(x_test)
-num <- n_test - 1
-den <- n_test - p - 1
-RSS_l <- sum((y_test - y_hat_l)^2)
-TSS <- sum((y_test-mean(y_test))^2)
+mod_lars2 <- lars(x_standardizer(x_train), y_standardizer(y_train))
 
-R2Adj_l <- 1-((num/den)*(RSS_l/TSS)) 
-# 1-((RSS/den)/(TSS/num)) # forumula prof
-R2Adj_l
-
-
-
-# Cross Validation Lars
-n <- nrow(real2)
-K = 10
-folds <- sample( rep(1:K,length=n) )
-KCV <- vector()
-
-for (k in 1:K) {
-  testIndexes <- which(folds==k,arr.ind=TRUE)
-  testData <- real2[testIndexes, ]
-  trainData <- real2[-testIndexes, ]
-  mod_lcv <- train(as.matrix(trainData[-1]), as.matrix(trainData[1]), lars)
-  y_hat_lcv <- mod_lcv(as.matrix(testData[-1]))$prediction
-  KCV[k] <- mean((as.matrix(testData[1])-y_hat_lcv)^2)
-}
-mean(KCV) # mse
-
-
-
-
-### Cross validation + feature selection LARS
-
-n <- nrow(real2)
-K = 10
-folds <- sample( rep(1:K,length=n) )
-KCV <- matrix(NA,K,100)
-
-for (k in 1:K) {
-  testIndexes <- which(folds==k,arr.ind=TRUE)
-  testData <- real2[testIndexes, ]
-  trainData <- real2[-testIndexes, ]
-  x_train <- as.matrix(trainData[,-1])
-  y_train <- as.matrix(trainData[,1])
-  x_test <- as.matrix(testData[,-1])
-  y_test <- as.matrix(testData[,1])
-  x_standardizer <- standardizer(x_train)
-  y_standardizer <- standardizer(y_train)
-  mod_lcv <- lars(x_standardizer(x_train), y_standardizer(y_train))
-  beta_compl <- mod_lcv$log
-  beta_sing <- mod_lcv$coef
-  x_test_stand <- x_standardizer(x_test)
-  I = ncol(beta_compl)
-  
-  for (i in 1:I) {
-    yhat_k <- x_test_stand %*% beta_compl[,i]
-    yhat_k <- y_standardizer(yhat_k, reverse = TRUE)
-    KCV[k,i] <- mean((y_test-yhat_k)^2)
-  }
-  
-}
-
-
-KCVmean<-apply(KCV,2,mean)
-KCVsd<-apply(KCV,2,sd)
-KCVmean <-na.omit(KCVmean)
-KCVsd <- na.omit(KCVsd)
-
-i1 <- which.min(KCVmean) # modello minCV
-i2 <- min(which(KCVmean<=KCVmean[i1]+KCVsd[i1]/sqrt(K))) # modello min one standard error rule
-
-plot(0:55,KCVmean, type="b",xlab="p", ylab="CV Error", pch=19,cex=.7)
-lines(0:55,KCVmean+KCVsd/sqrt(K),lty=2,col="darkred")
-lines(0:55,KCVmean-KCVsd/sqrt(K),lty=2,col="darkred")
-
-###########
-
-##### Ridge
-
-
-ridge_mse <- c()
-ridge_r2adj <- c()
-lambdas <- seq(1,2000,50)
-
-for (lambda in lambdas) {
-  mod_r <- train(x_train, y_train, ridge, lambda=lambda)
-  y_hat_r <- mod_r(x_test)$prediction
-  mse_r <- mean((y_test-y_hat_r)^2)
-  ridge_mse <- c(ridge_mse, mse_r)
-  RSS_r <- sum((y_test - y_hat_r)^2)
-  R2Adj_r <- 1-((num/den)*(RSS_r/TSS)) 
-  ridge_r2adj <- c(ridge_r2adj, R2Adj_r)
-}
-
-plot(lambdas,ridge_mse,type="l",ylim=c(4,4.8),xlim=c(0,2000),
-     xlab="Lambda",ylab="MSE",col="black")
-abline(h=mse.lars,lty=2)
-legend("topleft", lty=c(1),
-       legend=c("Ridge Regression"))
-
-#con ggplot
-ggplot() + geom_line( aes(x = lambdas, y = ridge_mse), color='red', lwd=1.2) + theme_minimal() +
-xlab("Lambda") + ylab("MSE") + xlim(0,10000) + ylim(1.9,3)
-
-
-#######
-
-#Prove per trovare lambda ottimo nel RIDGE con glmnet
-
-
-library(glmnet)
-fit.ridge = glmnet(x_train,y_train,family="gaussian",alpha=0)
-
-K <- 10
-ridge.cv<-cv.glmnet(x_train,y_train,alpha=0, nfolds = K, grouped=FALSE)
-plot(ridge.cv) # la seconda barra corrisponde al minimo errore di cross validation + una volta il suo standard error
-hatlambda <-ridge.cv$lambda.min
-hatlambda
-
-predict(fit.ridge, s=hatlambda, type = "coefficients")
-yhat.ridge = predict(fit.ridge, s=hatlambda, newx=x_test, exact=T)
-mse_ridge <- mean((y_test-yhat.ridge)^2)
-mse_ridge
-
-
-mod_r <- train(x_train, y_train, ridge, lambda=hatlambda)
-y_hat_r <- mod_r(x_test)$prediction
-mse_r <- mean((y_test-y_hat_r)^2)
-mse_r
-
-################# Grafici #################
-
-# per vederli meglio su windows
-trace(grDevices:::png, quote({
-  if (missing(type) && missing(antialias)) {
-    type <- "cairo-png"
-    antialias <- "subpixel"
-  }
-}), print = FALSE)
-
-
-##
-## Corrplot
-##
-
-# da valutare, le variabili sono moltissime, qua ci sono quelle con la correlazione più alta con la y e quelle più correlate tra loro
-
-
-corr <- cor(real2)
-
-corr[lower.tri(corr,diag=TRUE)] <- NA 
-corr[corr == 1] <- NA 
-corr <- as.data.frame(as.table(corr))
-corr <- na.omit(corr) 
-corr2 <- corr[corr$Var1=='y' ,] 
-corr2 <- subset(corr2, abs(Freq) > 0.3) 
-corr3 <- subset(corr, abs(Freq) > 0.98) # la soglia Ã¨ cosÃ¬ alta per limitare il numero di variabili nel grafico
-corrp <- rbind(corr2, corr3)
-mtx_corrp <- t(reshape2::acast(corrp, Var1~Var2, value.var="Freq"))
-ggcorrplot(mtx_corrp, lab = TRUE, ggtheme = ggplot2::theme_gray)
-
-
-##
-## Osservati vs predetti
-##
-
-db <- data.frame(seq(1:nrow(y_test)),cbind(y_test, y_hat_l))
-names(db) <- c("index","y_test", "y_hat")
-
-ggplot() + 
-  geom_line(data = db, aes(x = index, y = y_hat_l, color = "#00AFBB"), lwd=1.1) +
-  geom_line(data = db, aes(x = index, y = y_test, color = "#FC4E07"), lwd=1.1) +
-  xlab("Index") +
-  ylab('Values') +
-  theme_minimal() +
-  labs(title = "Observed values vs Predicted values") +
-  scale_color_discrete(name = "Values", labels = c("Predicted", "Observed"))
-
-
-
-db <- data.frame(seq(1:nrow(y_test)),cbind(y_test, y_hat_r))
-names(db) <- c("index","y_test", "y_hat")
-
-ggplot() + 
-  geom_line(data = db, aes(x = index, y = y_hat_r, color = "#00AFBB"), lwd=1.1) +
-  geom_line(data = db, aes(x = index, y = y_test, color = "#FC4E07"), lwd=1.1) +
-  xlab("Index") +
-  ylab('Values') +
-  theme_minimal() +
-  labs(title = "Observed values vs Predicted values") +
-  scale_color_discrete(name = "Values", labels = c("Predicted", "Observed"))
-
-
-
-
-
-
-##
-## grafici LARS
-##
-
-
-## presa da train()
-standardizer <- function(x) {
-    mu <- colMeans(x)
-    sigma <- as.double(sqrt(diag(var(x))))
-    function(y, reverse = FALSE) {
-        if (reverse) {
-            I <- diag(length(sigma)) * sigma
-            t(t(y %*% I) + mu)
-        } else {
-            I <- diag(length(sigma)) / sigma
-            t(t(y) - mu) %*% I
-        }
-    }
-}
-
-
-## usa tutti i dati
-x_train <- as.matrix(select(real2, -y))
-y_train <- as.matrix(select(real2,  y))
-
+beta_compl <- mod_lars2$log
+beta_sing <- mod_lars2$coef
+x_test_stand <- x_standardizer(x_test)
 x_standardizer <- standardizer(x_train)
 y_standardizer <- standardizer(y_train)
 
-mod <- lars(x_standardizer(x_train),
-            y_standardizer(y_train))
+beta_sin <- order(abs(beta_sing), decreasing = TRUE)
+beta_sin <- beta_sin[1:10]
+beta_compl <- beta_compl[beta_sin,]
 
-## rimuovi i coefficienti nulli
-betas <- mod$log
-betas[betas == 0] <- NA
+beta_compl[beta_compl == 0] <- NA
 
-selected_variables <- rowSums(is.na(betas)) < ncol(betas)
-betas <- betas[selected_variables, ]
-rownames(betas) <- colnames(x_train)[selected_variables]
+
+selected_variables <- rowSums(is.na(beta_compl)) < ncol(beta_compl) # se il numero di na è maggiore del numero di colonne
+beta_compl <- beta_compl[selected_variables, ]
+#rownames(betas) <- colnames(x_train)[selected_variables]
 
 
 ## rigira i dati in un formato facilmente interpretabile da ggplot
 data_plot <- tibble(
-    variable = c(),
-    value = c(),
-    iteration = c())
+    variable = numeric(),
+    value = numeric(),
+    iteration = numeric())
 
-p <- ncol(betas)
-for (i in seq_len(nrow(betas))) {
+p <- ncol(beta_compl)
+for (i in seq_len(nrow(beta_compl))) {
     data_plot <- data_plot %>%
-        add_row(variable = rownames(betas)[i],
-                value = betas[i, ],
+        add_row(variable = i,
+                value = beta_compl[i, ],
                 iteration = 1:p)
 }
 
+data_plot$variable <- as.character(data_plot$variable)
 
 ## plot dei risultati
 (ggplot(data_plot, aes(x = iteration, y = value, colour = variable)) +
     geom_line() + geom_hline(yintercept = 0) +
     theme_minimal() + theme(legend.position = "none") +
-    xlab("Iteration") + ylab("coefficient") +
-    ggtitle("LARS coefficient")) %>%
-
-    ggsave(filename = "caso_reale_lars.png", device = png(),
-           dpi = 500, width = 16, height = 9)
+    xlab("Iteration") + ylab("Coefficient") +
+    ggtitle("LARS coefficient"))                     
+                     
+                     
